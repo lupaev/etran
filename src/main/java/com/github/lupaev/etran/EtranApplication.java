abcbec689f7e37361c2318c0ba975158b19f0f8a
sergej <lupaevsv@gmail.com>
2025-02-11 20:51:38 +0700

    init

---
+++ src/main/java/com/github/lupaev/etran/EtranApplication.java
@@ -0,0 +1,13 @@
+package com.github.lupaev.etran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lupaev/etran/EtranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lupaev.etran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
